3042da27242d6a3352d36ffdccd22b36e9a61f62
2109921786 <126470165+2109921786@users.noreply.github.com>
2023-06-05 14:40:03 +0000

    first

---
+++ draw.py
@@ -0,0 +1,12 @@
+import turtle 
+ turtle . setup (800,800)＃调整画板大小 turtle . up ()＃提起笔
+ turtle . goto (-50,50)＃调整画板起始点 turtle . down ()＃放下笔
+ rtle . color (" black ")＃画笔颜色 turtle . speed (3)＃控制绘画速度＃画图过程
+ turtle . forward (100) turtle . left (40)
+ turtle . forward (70) turtle . left (160) turtle . forward (70) turtle . right (20) turtle . forward (50) turtle . right (40) turtle . forward (70) turtle . left (140) turtle . forward (70) turtle . right (10) turtle . up ()
+ turtle . forward (150) turtle . down ()
+ turtle . left (80)
+ turtle . forward (100) turtle . right (50) turtle . forward (120) turtle . left (50)
+ turtle . forward (100) turtle . right (150) turtle . forward (138) turtle . right (80)
+ turtle . forward ((140)) turtle . left (12)
+ turtle . forward (100)
---
+++ paintng.py
@@ -0,0 +1,11 @@
+canvas = turtle . Screen (
+#创建画笔
+ pen = turtle . Turtle ()
+＃设置画笔的颜色和线条宽度 pen . color (" red ")
+ pen . pensize (2)
+＃绘制五角星
+白 for i in range (5):
+ pen . forward (100)
+ pen . right (144)
+#隐藏画笔
+ pen . hideturtle
